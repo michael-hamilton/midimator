b3ee577daa7b5e35fbf89e9519c10adb93f9e0f4
Michael Hamilton <michaelhamilton626@gmail.com>
2020-11-22 11:15:17 -0500

    adds icon resources

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 const server = require('./server');
 
 function createWindow () {
@@ -7,6 +8,7 @@ function createWindow () {
     height: 150,
     minHeight: 150,
     minWidth: 750,
+    icon: path.resolve(`${__dirname}/resources/icon.ico`),
     webPreferences: {
       nodeIntegration: true,
     }
